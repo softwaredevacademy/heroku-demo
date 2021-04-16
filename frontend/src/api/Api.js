36735f229096bb88b79a1558846d5add8ff2cc6a
Kwabena <kwabena.a92@gmail.com>
2021-04-16 16:52:14 +0200

    Add production settings and script for frontend

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import Auth from "../services/Auth";
 
-const BASE_URL = "http://localhost:8080";
+const production  = 'https://young-garden-72293.herokuapp.com';
+const development = 'http://localhost:8080';
+const BASE_URL = (process.env.NODE_ENV ? production : development);
 
 const Api = axios.create({
     baseURL: BASE_URL,
